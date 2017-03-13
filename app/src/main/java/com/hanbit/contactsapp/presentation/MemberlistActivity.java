c0f723e21fd916637e4ae11d44073fc92e7cc284
rufruv <rufruv@gmail.com>
2017-03-13 12:08:42 +0900

    2017-03-13 12:08 draft complete !!

--- app/src/main/java/com/hanbit/contactsapp/presentation/MemberlistActivity.java
+++ app/src/main/java/com/hanbit/contactsapp/presentation/MemberlistActivity.java
@@ -6,7 +6,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hanbit.contactsapp.R;
@@ -21,6 +27,7 @@ public class MemberlistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memberlist);
+        ListView mList= (ListView) findViewById(R.id.mList);
         final MemberBean member = new MemberBean();
         findViewById(R.id.btGo).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +49,7 @@ public class MemberlistActivity extends AppCompatActivity {
         });
     }
     class MemberList extends ListQuery{
-        public MemberList(Context context) {
+         public MemberList(Context context) {
             super(context);
         }
         @Override
@@ -67,5 +74,65 @@ public class MemberlistActivity extends AppCompatActivity {
             }
             return list;
         }
+    }
+    class MemberAdapter extends BaseAdapter {
+        ArrayList<?>list;
+        LayoutInflater inflater;
+        private int[] photos={
+                R.drawable.cupcake,
+                R.drawable.donut,
+                R.drawable.eclair,
+                R.drawable.froyo,
+                R.drawable.gingerbread,
+                R.drawable.honeycomb,
+                R.drawable.icecream,
+                R.drawable.jellybean,
+                R.drawable.kitkat,
+                R.drawable.lollipop
+        };
+
+        public MemberAdapter(ArrayList<?> list, Context context) {
+            this.list = list;
+            this.inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return list.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override  //외부에서 받은 것을 편집해서 return하는 것이 Adapter
+        public View getView(int i, View v, ViewGroup g) {
+            ViewHoler holder;
+            if(v==null){
+                v=inflater.inflate(R.layout.member_item,null);
+                holder=new ViewHoler();
+                holder.profileImg= (ImageView) v.findViewById(R.id.profileImg);
+                holder.tvName= (TextView) findViewById(R.id.tvName);
+                holder.tvPhone= (TextView) findViewById(R.id.tvPhone);
+                v.setTag(holder);
+            }else{
+                holder= (ViewHoler) v.getTag();
+            }
+            holder.profileImg.setImageResource(photos[i]);
+            holder.tvName.setText(((MemberBean) list.get(i)).getName());
+            holder.tvPhone.setText(((MemberBean) list.get(i)).getPhone());
+            return v;
+        }
+    }
+    static class ViewHoler{
+        ImageView profileImg;
+        TextView tvName;
+        TextView tvPhone;
     }
 }
