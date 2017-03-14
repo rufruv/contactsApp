6732fb50358ab594f66a030b8639d47aaec42400
rufruv <rufruv@gmail.com>
2017-03-15 08:34:21 +0900

    2107-03-14 added commit

---
+++ app/src/main/java/com/hanbit/contactsapp/dao/UpdateQuery.java
@@ -0,0 +1,24 @@
+package com.hanbit.contactsapp.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by hb2002 on 2017-03-15.
+ */
+
+public abstract class UpdateQuery extends QueryFactory{
+    SQLiteOpenHelper helper;
+
+    public UpdateQuery(Context context) {
+        super(context);
+        helper = new DatabaseHelper(context);
+    }
+
+    @Override
+    public SQLiteDatabase getDatabase() {
+        return helper.getWritableDatabase();
+    }
+    public abstract void update(String sql);
+}
--- app/src/main/java/com/hanbit/contactsapp/presentation/MemberupdateActivity.java
+++ app/src/main/java/com/hanbit/contactsapp/presentation/MemberupdateActivity.java
@@ -1,6 +1,7 @@
 package com.hanbit.contactsapp.presentation;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -8,7 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.hanbit.contactsapp.R;
 import com.hanbit.contactsapp.dao.DetailQuery;
+import com.hanbit.contactsapp.dao.UpdateQuery;
 import com.hanbit.contactsapp.domain.MemberBean;
+import com.hanbit.contactsapp.service.DetailService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MemberupdateActivity extends AppCompatActivity {
 
@@ -16,23 +22,21 @@ public class MemberupdateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memberupdate);
-        /*Intent intent = this.getIntent();
+        Intent intent = this.getIntent();
+        final Map<String,String>map = new HashMap<>();
         final String id = intent.getExtras().getString("id");
-        Map<String, String> map = new HashMap<>();
-        map.put("id", id);
-        final MemberDetail mDetail = new MemberDetail(this);
-        UpdateService service = new UpdateService() {
+        final MemberDetail detail = new MemberDetail(MemberupdateActivity.this);
+        final MemberUpdate update = new MemberUpdate(MemberupdateActivity.this);
+        DetailService service = new DetailService() {
             @Override
-            public void update(Object o) {
-                MemberBean temp = (MemberBean) mDetail.findOne(
-                        "UPDATE Member SET phone,address,salary " +
-                );
+            public Object findOne(Map<?, ?> map) {
+                String sql = String.format("","");
+                MemberBean member = (MemberBean) detail.findOne(sql);
+                return member;
             }
-        };*/
-
-
+        };
     }
-    class MemberDetail extends DetailQuery {
+    class MemberDetail extends DetailQuery{
 
         public MemberDetail(Context context) {
             super(context);
@@ -40,22 +44,38 @@ public class MemberupdateActivity extends AppCompatActivity {
 
         @Override
         public Object findOne(String sql) {
-            MemberBean bean=null;
-            SQLiteDatabase db=super.getDatabase();
-            Cursor cursor=db.rawQuery(sql,null);
-
+            MemberBean bean = null;
+            SQLiteDatabase db = super.getDatabase();
+            Cursor cursor = db.rawQuery(sql,null);
             if(cursor!=null){
                 if(cursor.moveToNext()){
-                    bean=new MemberBean();
-                    bean.setId(cursor.getString(cursor.getColumnIndex("id")));
-                    bean.setName(cursor.getString(cursor.getColumnIndex("name")));
-                    bean.setPhone(cursor.getString(cursor.getColumnIndex("phone")));
-                    bean.setAge(cursor.getString(cursor.getColumnIndex("age")));
-                    bean.setAddress(cursor.getString(cursor.getColumnIndex("address")));
-                    bean.setSalary(cursor.getString(cursor.getColumnIndex("salary")));
+                    bean = new MemberBean();
+                    String id = cursor.getString(cursor.getColumnIndex("id"));
+                    String name = cursor.getString(cursor.getColumnIndex("name"));
+                    String age = cursor.getString(cursor.getColumnIndex("age"));
+                    String phone = cursor.getString(cursor.getColumnIndex("phone"));
+                    String address = cursor.getString(cursor.getColumnIndex("address"));
+                    String salary = cursor.getString(cursor.getColumnIndex("salary"));
+                    bean.setId(id);
+                    bean.setName(name);
+                    bean.setAddress(address);
+                    bean.setAge(age);
+                    bean.setPhone(phone);
+                    bean.setSalary(salary);
                 }
             }
             return bean;
+        }
+    }
+    class MemberUpdate extends UpdateQuery{
+
+        public MemberUpdate(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void update(String sql) {
+            super.getDatabase().execSQL(sql);
         }
     }
 }
